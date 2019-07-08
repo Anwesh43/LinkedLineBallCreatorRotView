dc295a1c0d4e0c53c69b61b98c43d1f34af4b099
Anwesh43 <anweshthecool0@gmail.com>
2019-07-08 08:36:03 +0530

    adding  canvas extension methods

--- lineballcreatorrotview/src/main/java/com/anwesh/uiprojects/lineballcreatorrotview/LineBallCreatorRotView.kt
+++ lineballcreatorrotview/src/main/java/com/anwesh/uiprojects/lineballcreatorrotview/LineBallCreatorRotView.kt
@@ -13,13 +13,14 @@ import android.graphics.Canvas
 import android.graphics.Color
 
 val nodes : Int = 5
-val balls : Int = 3
+val balls : Int = 5
 val scGap : Float = 0.05f
 val scDiv : Double = 0.51
 val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#1A237E")
 val backColor : Int = Color.parseColor("#BDBDBD")
+val rotDeg : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
@@ -30,3 +31,39 @@ fun Float.mirrorValue(a : Int, b : Int) : Float {
     return (1 - k) * a.inverse() + k * b.inverse()
 }
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawBall(i : Int, sc : Float, size : Float, paint : Paint) {
+    val sci : Float = sc.divideScale(i, balls)
+    val sci1 : Float = sci.divideScale(0, 2)
+    val sci2 : Float = sci.divideScale(1, 2)
+    val r : Float = size / balls
+    val origY : Float = -size
+    val destY : Float = size - r - 2 * r * i
+    save()
+    translate(0f, origY + (destY - origY) * sci2)
+    drawCircle(0f, 0f, r * sci1, paint)
+    restore()
+}
+
+fun Canvas.drawBalls(sc : Float, size : Float, paint : Paint) {
+    for (j in 0..(balls - 1)) {
+        drawBall(j, sc, size, paint)
+    }
+}
+
+fun Canvas.drawLBCRNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.color = foreColor
+    save()
+    translate(gap * (i + 1), h / 2)
+    rotate(rotDeg * sc2)
+    drawBalls(sc1, size, paint)
+    restore()
+}
