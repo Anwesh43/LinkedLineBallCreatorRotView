54683b221446c0114214d25277c4b3c86bb5c877
Anwesh43 <anweshthecool0@gmail.com>
2019-07-08 08:57:24 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedlineballcreatorrotview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedlineballcreatorrotview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedlineballcreatorrotview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.lineballcreatorrotview.LineBallCreatorRotView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LineBallCreatorRotView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
