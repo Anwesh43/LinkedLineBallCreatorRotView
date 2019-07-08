40f1a5fed6aea459c6b8c8a4e74f26f1d3a1cd0e
Anwesh43 <anweshthecool0@gmail.com>
2019-07-08 08:22:32 +0530

    adding constants and libraries

--- lineballcreatorrotview/src/main/java/com/anwesh/uiprojects/lineballcreatorrotview/LineBallCreatorRotView.kt
+++ lineballcreatorrotview/src/main/java/com/anwesh/uiprojects/lineballcreatorrotview/LineBallCreatorRotView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.lineballcreatorrotview
 /**
  * Created by anweshmishra on 08/07/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val balls : Int = 3
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#1A237E")
+val backColor : Int = Color.parseColor("#BDBDBD")
